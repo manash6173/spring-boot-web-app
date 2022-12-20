8206b32a00184a41d25402717790edd33611a450
Manash Kumar Swain <41792708+manash6173@users.noreply.github.com>
2022-12-20 13:04:34 +0530

    Update MyWebController.java

--- src/main/java/com/manash/learn/web/MyWebController.java
+++ src/main/java/com/manash/learn/web/MyWebController.java
@@ -2,16 +2,28 @@ package com.manash.learn.web;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyWebController {
+	
+	@Value("${wish}")
+	private String wishMsg;
+	
+	@Value("${secret}")
+	private String secreteMsg;
 
 	@GetMapping("/welcome/{name}")
 	public String welcomeMsg(@PathVariable("name") String name) {
-		return "<center><h1>Hello ," + name + ", Dockerized spring boot web application running successfully....</h1></center> " + new Date();
+		
+		String welcomeMsg = "<center><h1>" + name + ",  Welcome to spring boot web application ....</h1></center>"
+				+ "<h3>"+wishMsg+secreteMsg+"</h3>";
+		
+		
+		return welcomeMsg + new Date();
 	}
 
 }
